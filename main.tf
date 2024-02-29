30b609d1c933a2213c33cfba6209e811a5085c6a
Billy Gibendi <ralphgibendi01@gmail.com>
2024-02-29 17:30:01 +0300

    added tags

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ provider "google" {
 resource "google_compute_instance" "terraform" {
   name         = "terraform"
   machine_type = "e2-micro"
+  tags         = ["web", "dev"]
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
